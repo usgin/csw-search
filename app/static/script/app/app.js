17313ff3123428c9074e6fcc93d61a44f7a7ca26
Ryan Clark <ryan.clark.j@gmail.com>
2011-09-28 13:57:50 -0700

    More proxy adjustments

--- app/static/script/app/app.js
+++ app/static/script/app/app.js
@@ -6,7 +6,7 @@ Ext.onReady(function() {
         portalConfig: {
             layout: "border",
             region: "center",
-            proxy: "/proxy?url=",
+            proxy: "/proxy/?url=",
             
             // by configuring items here, we don't need to configure portalItems
             // and save a wrapping container
